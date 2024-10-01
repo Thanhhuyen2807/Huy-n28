e9cfd1a9f27390f50ed8cd6e331cb27d27ff76fa
Your Name <thanhuyenxt28@gmail.com>
2024-10-01 09:45:08 +0700

    commit note

--- obj/Debug/net8.0/demomvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/demomvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("demomvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c18d9b3ea59b2a833075aa3686f326d84827467")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6767cd116c3bc674954b68b1c97f4a303cf8dcbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("demomvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("demomvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
