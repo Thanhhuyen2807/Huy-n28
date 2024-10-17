d1a13e4228c50317670d907e6bc2a81a7dd9c964
ThanhHuyen <thanhuyenxt28@gmail.com>
2024-10-17 16:35:56 +0700

    Revert "add"
    This reverts commit 525652e2eb9bf39aa4f8924abadebd38de8a8f7c.

--- obj/Debug/net8.0/demomvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/demomvc.AssemblyInfo.cs
@@ -13,11 +13,8 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("demomvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+525652e2eb9bf39aa4f8924abadebd38de8a8f7c")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9cfd1a9f27390f50ed8cd6e331cb27d27ff76fa")]
->>>>>>> parent of fc2878c (commit note)
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de41ae98605ab94ebc15fa3a54227c34fab4d07a")]
 [assembly: System.Reflection.AssemblyProductAttribute("demomvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("demomvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
